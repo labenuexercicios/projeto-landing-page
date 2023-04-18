303ef1b349be37ed4b182ccbaa98be790e1bce25
anderson-nunes <andersonribeironunes@hotmail.com>
2023-04-18 13:36:40 -0300

    Refatorando a estilização

---
+++ src/components/menuToggle/index.js
@@ -0,0 +1,14 @@
+// // FUNÇÃO MENU HAMBURGUER
+
+// let show = true
+
+// const menuSection = document.querySelector('.menu-section')
+// const menuToggle = menuSection.querySelector('.menu-toggle')
+
+// menuSection.addEventListener('click', () => {
+//   document.body.style = show ? 'hidden' : 'initial'
+//   // overflow
+
+//   menuSection.classList.toggle('on', show)
+//   show = !show
+// })
