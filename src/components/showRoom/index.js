733868b8fbb16f4b6c5290a753c4f407b1a6abc1
anderson-nunes <andersonribeironunes@hotmail.com>
2023-05-02 09:49:03 -0300

    Apliquei a responsividade nas seções

--- src/components/showRoom/index.js
+++ src/components/showRoom/index.js
@@ -2,6 +2,7 @@
 // let modal_show = document.querySelector('.modal-show')
 // let modalImg = document.querySelector('#modal-img')
 // let btnClose = document.querySelector('#btn-close')
+// let overlayImg = document.querySelector('.overlay')
 // let srcVal = ""
 
 // for (let i = 0; i < imagens.length; i++) {
@@ -13,22 +14,23 @@
 
 //     modalImg.classList.remove('hidden')
 //     overlay.classList.remove('hidden')
-
-
 //   })
 // }
 
-// btnClose.addEventListener('click', function () {
+// function closeImageModal() {
 //   modal_show.classList.toggle('modal-active')
 
 //   modalImg.classList.add('hidden')
 //   overlay.classList.add('hidden')
-// })
+// }
 
-// // document.addEventListener('keydown', function (e) {
-// //   if (e.key === 'Escape' && !modalImg.classList.contains('hidden')) {
-// //     btnClose()
-// //   }
-// // })
+// btnClose.addEventListener('click', closeImageModal)
+// overlayImg.addEventListener('click', closeImageModal)
+
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !modalImg.classList.contains('hidden')) {
+//     closeImageModal()
+//   }
+// })
 
 
