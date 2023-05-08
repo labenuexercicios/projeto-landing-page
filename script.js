aa08802b3beb82a236df9255572ffe2c77463147
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-05-08 15:49:56 -0300

    att trabalho

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navLinks = document.querySelector('.lista');
+const links = document.querySelectorAll('.lista li');
+
+hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburguer.classList.toggle('active');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburguer.classList.remove('active');
+    });
+});
 
 
 function botaoLimpa() {
-    document.getElementById("nome"). value = ""
-    document.getElementById("email"). value = ""
+  document.getElementById("nome"). value = ""
+  document.getElementById("email"). value = ""
 }
-
-
