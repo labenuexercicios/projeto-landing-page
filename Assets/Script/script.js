4cc4aea88e0e54448a683203ff16c610e85f914b
Roberta2410 <138620335+Roberta2410@users.noreply.github.com>
2023-10-09 22:40:26 -0300

    Projeto finalizado

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -2,3 +2,22 @@ function irParaAbrirConta (){
 var SessaoAbrirConta = document.getElementById ("Container-abrir-conta");
 SessaoAbrirConta.scrollIntoView({behavior:"smooth"});
 }
+
+// script.js
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    var menuToggle = document.querySelector('.menu-toggle');
+  
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+  }
+  
+  document.addEventListener('click', function (e) {
+    var menu = document.querySelector('.menu');
+    var menuToggle = document.querySelector('.menu-toggle');
+  
+    if (!menu.contains(e.target) && e.target !== menuToggle) {
+        menu.classList.remove('active');
+  menuToggle.classList.remove('active');
+    }
+  });
